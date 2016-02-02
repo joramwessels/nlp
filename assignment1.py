8e284d8a8a244876a197ea5bfaa475ea7697b2f6
joramwessels <joram.wessels@outlook.com>
2016-02-02 16:31:55 +0100

    Done

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,55 @@
 #! /usr/bin/python
 
-import sys, argsparse
+"""
+NLP Assignment 1
+Amir Alnomani		10437797
+Maurits Offerhaus	1040036
+Joram Wessels		10631542
+
+Compiles using Python version 3.5 with the following command:
+	python assignment1.py -corpus [path] -n [value] -m [value]
+
+Comulative frequencies:
+	unigrams:	617091
+	bigrams:	617091
+	
+"""
+
+import sys, argparse, operator
 
 def main():
-	args = parseArgs(sys.argv[1:])
-	
+	filename, N, M = parseArgs(sys.argv[1:])
+	frequencies = countNGrams(filename, N, M)
+	sortedList = sorted(frequencies.items(), key=operator.itemgetter(1))
+	sortedList.reverse()
+	prettyPrint(sortedList, M)
+
+def countNGrams(filename, N, M):
+	ngram = nGramGenerator(filename, N)
+	frequencies = {}
+	for gram in ngram:
+		key = " ".join(gram)
+		if not key in frequencies.keys():
+			frequencies[key] = 1
+		else:
+			frequencies[key] += 1
+	return frequencies
+
+def nGramGenerator(filename, N):
+	with open(filename, 'r') as file:
+		buffer = [""]*N
+		for line in file:
+			sentence = line.strip().split(" ")
+			for word in sentence:
+				if (word != ''):
+					buffer.pop(0)
+					buffer.insert(N-1, word)
+					yield buffer
+
+def prettyPrint(list, M):
+	print("NGram\t\t\t\tFrequency")
+	for i in range(M):
+		print(list[i][0],"\t\t\t\t|",list[i][1])
 
 def parseArgs(args):
 	"""Parses the arguments using argparse.
@@ -32,10 +77,9 @@ def parseArgs(args):
 		"-m",
 		metavar='M',
 		type=int,
-		required=False,
 		help="The amount of most probable n-grams to print.")
-	parsed = parser.parse_args(argv)
-	return parsed.corpus, parsed.n, parsed.m
+	parsed = parser.parse_args(args)
+	return parsed.corpus[0], parsed.n, parsed.m
 
 if __name__ == "__main__":
 	main()
