9852335c2e0b8888a7c0af6022a503815932ff47
joramwessels <joram.wessels@outlook.com>
2016-02-12 12:52:39 +0100

    Q2 needs to be split into question2 and NGramProbabilities. Needs testing...

--- assignment2.py
+++ assignment2.py
@@ -12,7 +12,7 @@ Compiles using Python version 3.5 with the following command:
 
 """
 import sys, argparse, operator
-from NGrams import nGramGenerator, countNGrams
+from NGrams import countNGrams
 import question2_2 as q2
 import question2_3 as q3
 import question2_4 as q4
@@ -22,7 +22,7 @@ def main():
 	corpus = parsed["corpus"][0]
 	CPF = parsed["conditional_prob_file"][0]
 	SPF = parsed["sequence_prob_file"][0]
-	SP = parsed["scored_permutations"][0]
+	SP = parsed["scored_permutations"]
 	N = parsed["n"]
 	question1(corpus)
 	
@@ -35,14 +35,34 @@ def main():
 	question2(nGramProbs, CPF, N)
 	sentenceProb = sentenceProbabilities(NGramProbs, N)
 	question3(sentenceProb, SPF, N)
-	permutations = question4(sentenceProbs, SP)
-	print(nGramProbs, sentenceProbs, permutations)
+	question4(sentenceProbs, SP)
 
 def NGramProbabilities(N_freq, N_min_1_freq):
+	"""Constructs a dictionary mapping N-grams to their probability.
+	
+	Args:
+		N_freq (dict): A dictionary mapping N-grams to their frequency.
+		N_min_1_freq (dict): A dictionary mapping (N-1)-grams to their frequency.
+	Returns:
+		dict: A dictionary mapping N-grams to their probability.
+	
+	"""
 	NGramProbs = {}
+	# TODO
 	return NGramProbs
 
 def SentenceProbabilities(NGramProbs, N, start="START", stop="STOP"):
+	"""Constructs a closure calculating sentence probabilities.
+	
+	Args:
+		NGramProbs (dict): A dictionary mapping N-grams to their probability.
+		N (int): The N value of the N-grams.
+		start (str): (optional) The symbol for the start of a sentence.
+		stop (str): (optional) The symbol for the end of a sentence.
+	Returns:
+		func: A closure calculating the probability of a sentence.
+	
+	"""
 	def func(line):
 		words = [start]*(N-1) + line.strip().split(' ') + [stop]*(N-1)
 		words = [word for word in words if not word == '']
@@ -57,6 +77,14 @@ def SentenceProbabilities(NGramProbs, N, start="START", stop="STOP"):
 	return func
 
 def question1(filename, N=2, M=10):
+	"""Prints the M most frequent N-grams.
+	
+	Args:
+		filename (str): The name of the corpus to extract N-grams from.
+		N (int): (optional) The N value of the N-grams (bigrams are requested)
+		M (int): (optional) The amount of most N-grams printed (10 is requested)
+	
+	"""
 	freq = countNGrams(filename, N)
 	sortedList = sorted(freq.items(), key=operator.itemgetter(1))
 	sortedList.reverse()
@@ -64,18 +92,42 @@ def question1(filename, N=2, M=10):
 	prettyPrint(sortedList, M+1)
 
 def question2(NGramProbs, filename, N):
+	"""Prints the probabilities of the N-grams in the given file.
+	
+	Args:
+		NGramProbs (dict): A dictionary mapping N-grams to their probability.
+		filename (str): The name of the N-grams file to test.
+		N (int): The N value of the N-grams.
+	
+	"""
 	if (NGramProbs == None or filename == None): return
 	q2.probabilities(NGramProbs, filename, N)
 
 def question3(sentenceProb, filename, N):
+	"""Prints the probabilities of the sentences in the given file.
+	
+	Args:
+		sentenceProb (func): A closure calculating the probability of a sentence.
+		filename (str): The name of the file containing the test sentences.
+		N (int): The N value of the N-grams.
+	
+	"""
 	if (sentenceProb == None or filename == None): return
 	
 	with open(filename, 'r') as file:
-		probabilities = [sentenceProb(line) for line in file]
-	return probabilities
+		probabilities = [(line, sentenceProb(line)) for line in file]
+	printList.insert(0, ("Sentence", "Probability"))
+	prettyPrint(printList, len(printList))
 
-def question4(sentenceProb, filename):
-	if (sentenceProb == None or filename == None): return
+def question4(sentenceProb, scoredPermutations):
+	"""Prints the probabilities of the permutations of set A and B.
+	
+	Args:
+		sentenceProb (func): A closure calculating the probability of a sentence.
+		scoredPermutations (bool): Whether to print this question or not.
+	
+	"""
+	if (sentenceProb == None or not scoredPermutations): return
 	
 	setA = ["know","I","opinion","do","be","your","not","may","what"]
 	setB = ["I","do","not","know"]
@@ -88,9 +140,18 @@ def question4(sentenceProb, filename):
 		prob[permA] = sentenceProbs(permA)
 		permB = " ".join(permB)
 		prob[permB] = sentenceProbs(permB)
-	return prob
+	printList = list(prob)
+	printList.insert(0, ("Permutation", "Probability"))
+	prettyPrint(printList, len(printList))
 
 def prettyPrint(list, M):
+	"""Prints the tuples in the list as columns, with the labels as the first tuple.
+	
+	Args:
+		list (list): A list of tuples, which will be printed sequentially.
+		M (int): The amount of list entries to print.
+	
+	"""
 	slice = list[:M]
 	lengths = [max([len(str(tuple[i])) for tuple in slice]) for i in range(len(list[0]))]
 	for row in slice:
@@ -112,18 +173,18 @@ def parseArgs(args):
 	parser = argparse.ArgumentParser(description=name)
 	parser.add_argument(
 		"-corpus",
-		metavar='C',
+		metavar='[path]',
 		type=str,
 		nargs=1,
 		help="The path to the corpus.")
 	parser.add_argument(
 		"-n",
-		metavar='N',
+		metavar='[value]',
 		type=int,
 		help="The 'n' order of the n-grams.")
 	parser.add_argument(
 		"-conditional-prob-file",
-		metavar='CO',
+		metavar='[path]',
 		type=str,
 		nargs=1,
 		required=False,
@@ -131,7 +192,7 @@ def parseArgs(args):
 		help="The path to the conditional probability file.")
 	parser.add_argument(
 		"-sequence-prob-file",
-		metavar='SE',
+		metavar='[path]',
 		type=str,
 		nargs=1,
 		required=False,
@@ -139,11 +200,9 @@ def parseArgs(args):
 		help="The path to the sequence probability file.")
 	parser.add_argument(
 		"-scored-permutations",
-		metavar='SC',
 		type=str,
-		nargs=1,
+		nargs=0,
 		required=False,
-		default=[None],
 		help="The path to the scored permutations file.")
 	parsed = vars(parser.parse_args(args))
 	return parsed
