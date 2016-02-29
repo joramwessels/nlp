a4b61fc70cf1a67c3d6929cf0be505bef8d8a6b6
joramwessels <joram.wessels@outlook.com>
2016-02-29 16:45:11 +0100

    During testing. 'Too many values to unpack' and 'division by zero'

--- Part A/HMM.py
+++ Part A/HMM.py
@@ -1,26 +1,31 @@
 #! /usr/bin/python
 
+import sys
 from posgram import viterbiMatrices as freqs
 
 def main():
+	model = HMM(sys.argv[1])
+	print(model.getTransitionProbs('NN'))
+	print(model.getTagProbs('you'))
 	
+LE = ["./.", "======================================"]
+WE = [' ', '\n']
+TD = '/'
 
 class HMM:
 	
-	LE = ["./.", "======================================"]
-	WE = [' ', '\n']
-	TD = '/'
-	
 	def __init__(self, filename):
+		print("posgram")
 		tr, wt = freqs(filename, 2, line_ends=LE, word_ends=WE, tag_del=TD)
-		tr1,   = freqs(filename, 1, line_ends=LE, word_ends=WE, tag_del=TD)
+		print("posgram")
+		tr1, wt1 = freqs(filename, 1, line_ends=LE, word_ends=WE, tag_del=TD)
 		self.transitionProbs = freqsToProbs(tr, tr1)
 		self.wordTagProbs = indexTagProbs(wt)
 	
 	def getTransitionProbs(self, tag):
 		dict = self.transitionProbs
 		if tag in dict.keys():
-			return = dict[tag]
+			return dict[tag]
 		else:
 			return {}
 	
@@ -32,6 +37,7 @@ class HMM:
 			return {}
 
 def indexTagProbs(frequencies):
+	print("indexTagProbs")
 	probs = {}
 	for key, value in frequencies.items():
 		(word, tag) = key.split(TD)
@@ -41,16 +47,17 @@ def indexTagProbs(frequencies):
 			probs[word] = {tag : value}
 	normalized = {}
 	for word, tagProbs in probs.items():
-		total = sum([val for val in tagProbs.values()])
+		total = sum(list(tagProbs.values()))
 		normalized[word] = {}
 		for tag, prob in tagProbs.items():
 			normalized[word][tag] = prob / total
 	return normalized
 
 def freqsToProbs(biFreqs, uniFreqs):
+	print("freqsToProbs")
 	gtProbability = turingSmoothing(biFreqs, uniFreqs)
 	probs = {}
-	for key, value in frequencies.items():
+	for key, value in biFreqs.items():
 		(tag1, tag2) = key.split(WE[0])
 		if tag1 in probs.keys():
 			probs[tag1][tag2] = gtProbability(key)
@@ -59,6 +66,7 @@ def freqsToProbs(biFreqs, uniFreqs):
 	return probs
 
 def turingSmoothing(Nfreq, N1freq):
+	print("turingSmoothing")
 	"""Returns a closure that calculates probability using Turing Smoothing.
 	
 	Args:
@@ -70,8 +78,10 @@ def turingSmoothing(Nfreq, N1freq):
 	"""
 	Nvalues = list(Nfreq.values())
 	N1values = list(N1freq.values())
-	N = dict([(i, Nvalues.count(i)) for i in range(max(Nvalues))])
-	N1 = dict([(i, N1values.count(i)) for i in range(max(N1values))])
+	print("\tN")
+	N = dict([(i, Nvalues.count(i)) for i in Nvalues])
+	print("\tN1")
+	N1 = dict([(i, N1values.count(i)) for i in N1values])
 	def probability(ngram):
 		n1gram = ngram.split(' ')[:-1]
 		PN = gts(ngram, N, Nfreq)
@@ -81,10 +91,22 @@ def turingSmoothing(Nfreq, N1freq):
 	return probability
 
 def gts(ngram, N, Nfreq):
+	"""Performs the GTS calculations. 'C' is the frequency of 'ngram',
+		Nc is the amount of ngrams with frequency 'C'.
+	
+	Args:
+		ngram (str): The N-gram of which to calculate the prediction.
+		N (dict): A dict mapping ngram frequencies to "frequency frequencies".
+		Nfreq (dict): A dict mapping N-grams to their frequency.
+	Returns:
+		float: The GT-smoothed probability of the given N-gram.
+	
+	"""
 	C = frequency(Nfreq, ngram)
 	if C == 0:
 		return frequency(N, 1) / sum(N.values())
 	Nc = frequency(N, C)
+	if Nc == 0: print(C, ngram, (ngram in Nfreq))
 	Nc1 = frequency(N, C+1)
 	return ((C + 1) * Nc1) / Nc
 
