d1001e5bf955db980ff9ed3cc9da4b2a9bf609f8
joramwessels <joram.wessels@outlook.com>
2016-03-16 22:54:30 +0100

    Implementation allowing different 'h' and 'v' values. Bug: predicates with more than two branches are troublesome to expand

--- Part B/parse.py
+++ Part B/parse.py
@@ -1,47 +1,57 @@
 
-import re
-#(ROOT (S (NP (DT A) (NN record) (NN date)) (VP (VBZ has) (RB n't) (VP (VBN been) (VP (VBN set)))) (. .)))
-
-sentence = ['NP', ['DT', 'A'], ['NN', 'record'], ['NN','date']]
+sentence = ['S', ['NP', ['DT', 'A'], ['NN', 'record'], ['NN','date']]]
 sentence2 = ['NP', ['NNP', 'Rolls Royce'], ['NNP', 'motor'], ['NNPS','cars'],['NNP','Inc']]
 sentence3 = ['NP', ['DT', 'A'], ['NN', 'record'], ['NN','date'],['NP', ['NNP', 'Rolls Royce'], ['NNP', 'motor'], ['NNPS','cars'],['NNP','Inc']]]
-#[NP, [DT, A],[@NP->DT, [NN, record],[ '@NP->_DT_NN' , ['NN' , 'date']]]]
+sentence4 = ['ROOT', ['S', ['NP', ['NNP', 'Ms.'], ['NNP', 'Haag']], ['VP', ['VBZ', 'plays'], ['NP', ['NNP', 'Elianti']]], ['.', '.']]]
 
-#for element in sentence:
-#	if isinstance(element, list) and element[0].isupper():
+hValue = 2
+vValue = 2
 
+def binarize(sent, v, h):
+	"""
+	[POS word] -> [POS word]
+	[POS2 Non-Terminal] -> [POS2^POS1 binarize(Non-Terminal)]
+	[POS2 NT1 NT2] -> [POS2^POS1 binarize(NT1) [@POS2^POS1->_NT1 binarize(NT2)]]
+	[POS2 NT1 NT2 NT3] -> [POS2^POS1 NT1 [POS3^POS2 NT2 NT3]]
+	"""
+	if isTerminal(sent): return sent
+	print(sent)
+	predicates = [sent[0]] + v[:-1]
+	variables  = h[1:] + [sent[1][0]]
+	rightLabel = '@' + joinLabels('^', predicates) + "->_" + joinLabels('_', variables)
+	leftNode = binarize(sent[1], predicates, ['']*hValue)
+	if len(sent) == 2: return [joinLabels('^', predicates), binarize(sent[1], predicates, ['']*hValue)]
+	elif len(sent) == 3: rightNode = [rightLabel, binarize(sent[2], predicates, variables)]
+	elif len(sent) > 3: rightNode = [rightLabel, binarize([sent[0]] + sent[2:], v, variables)]
+	return [joinLabels('^', predicates), leftNode, rightNode]
 
+def joinLabels(sep, list):
+	"""Joins a list, excluding empty strings.
+	"""
+	return sep.join([x for x in list if x != ''])
 
+def isTerminal(sent):
+	"""Checks if a (sub-)sentence is a terminal symbol.
+	"""
+	if any([not (isinstance(x, str) or isinstance(x, list)) for x in sent]):
+		raise TypeError("Incompatible data type encountered.")
+	elif len(sent) == 2:
+		if isinstance(sent[0], str) and isinstance(sent[1], str): return True
+		return False
+	elif len(sent) > 2:
+		return False
+	else:
+		raise Exception("Sentence of length '%d' encountered: '%s'" %(len(sent), sent))
 
-def binarize(sentence):
-	Path = "@" + sentence[0] + "->_"
-	branches = sentence[1:]
-	branchLength = len(branches)
-	branches = [branches[i][0] for i in range(branchLength)]
-	
-	if(not sentence[0].isalpha() or not isinstance(sentence[1],list)):
-		return sentence
-	if(branchLength == 2):
-		return [sentence[0], binarize(sentence[1]) ,binarize(sentence[1])]
-	if(branchLength == 1):
-		return [sentence[0], binarize(sentence[1])]
+def printTree(sent, depth):
+	"""Prints a sentence as an indented tree.
+	"""
+	for i in range(depth): print(' ', end='')
+	if isinstance(sent, str):
+		print(sent)
+	else:
+		print(sent[0])
+		for x in sent[1:]: printTree(x, depth + 1)
 
-	def b(Path ,i):
-		currentLoc = branches[i-2]
-		if( i == branchLength):
-			newPath = Path + '_' + currentLoc 
-			binarizedSent = binarize(sentence[i])
-			return [newPath , binarizedSent]
-		else:
-			i += 1
-			newPath = Path + '_' + currentLoc
-			binarizedSent = binarize(sentence[i-1])
-			return [ newPath, binarizedSent] + [b(newPath,i)]
-			
-	return sentence[:2]+[b(Path,2)]
-
-
-
-print(binarize(sentence3))
-
-
+print(binarize(sentence4, ['']*vValue, ['']*hValue))
+printTree(binarize(sentence4, ['']*vValue, ['']*hValue), 0)
