f62565b324c057447c4e7593467d4fa6920a8d88
joramwessels <joram.wessels@outlook.com>
2016-03-17 10:17:47 +0100

    Working, Tested & Commented

--- Part B/parse.py
+++ Part B/parse.py
@@ -7,23 +7,43 @@ sentence4 = ['ROOT', ['S', ['NP', ['NNP', 'Ms.'], ['NNP', 'Haag']], ['VP', ['VBZ
 hValue = 2
 vValue = 2
 
-def binarize(sent, v, h):
+def binarize(sent, v=['']*vValue, h=['']*hValue):
+	"""Binarizes a sentence using both vertical- and horizontal Markovization of
+		the orders specified by the 'vValue' and 'hValue' variables respectively.
+	
+	Note:
+		[POS word] -> [POS word]
+		[POS2 Non-Terminal] -> [POS2^POS1 binarize(Non-Terminal)]
+		[POS2 NT1 NT2] -> [POS2^POS1 binarize(NT1) [@POS2^POS1->_NT1 binarize(NT2)]]
+		[POS2 NT1 NT2 NT3] -> [POS2^POS1 NT1 [POS3^POS2 NT2 NT3]]
+	Args:
+		sent (list): The sentence to be binarized, represented as a list of lists.
+		v (list): (optional) The vertical Markovization queue that keeps track
+			of the parent node POS tags. It is only used in recursive steps.
+		h (list): (optional) The horizontal Markovization queue that keeps track
+			of the sibling node POS tags. It is only used in recursive steps.
+	Returns:
+		list: The binarized sentence, represented as a list of lists.
+	
 	"""
-	[POS word] -> [POS word]
-	[POS2 Non-Terminal] -> [POS2^POS1 binarize(Non-Terminal)]
-	[POS2 NT1 NT2] -> [POS2^POS1 binarize(NT1) [@POS2^POS1->_NT1 binarize(NT2)]]
-	[POS2 NT1 NT2 NT3] -> [POS2^POS1 NT1 [POS3^POS2 NT2 NT3]]
-	"""
+	# terminal symbols are the basecase
 	if isTerminal(sent): return sent
-	print(sent)
-	predicates = [sent[0]] + v[:-1]
-	variables  = h[1:] + [sent[1][0]]
-	rightLabel = '@' + joinLabels('^', predicates) + "->_" + joinLabels('_', variables)
-	leftNode = binarize(sent[1], predicates, ['']*hValue)
-	if len(sent) == 2: return [joinLabels('^', predicates), binarize(sent[1], predicates, ['']*hValue)]
-	elif len(sent) == 3: rightNode = [rightLabel, binarize(sent[2], predicates, variables)]
-	elif len(sent) > 3: rightNode = [rightLabel, binarize([sent[0]] + sent[2:], v, variables)]
-	return [joinLabels('^', predicates), leftNode, rightNode]
+	# add the new POS tags to the vertical- and horizontal Markovization queues
+	vert = [sent[0]] + v[:-1]
+	hori  = h[1:] + [sent[1][0]]
+	# determine the POS tag of the right node using Markovization
+	rightLabel = '@' + joinLabels('^', vert) + "->_" + joinLabels('_', hori)
+	# clearing the horizontal Markovization queue prior to binarizing the left node
+	leftNode = binarize(sent[1], v=vert)
+	# upon encountering a unary node, clear the horizontal Markovization queue and binarize the first element
+	if len(sent) == 2: return [joinLabels('^', vert), binarize(sent[1], v=vert)]
+	# upon encountering a binary node, binarize the right node using the horizontal Markovization queue
+	elif len(sent) == 3: rightNode = [rightLabel, binarize(sent[2], v=vert, h=hori)]
+	# upon encountering more than 2 nodes, binarize every node after index 1 using the same
+	# Markovization state as the current node and append the content to the new POS tag
+	elif len(sent) > 3: rightNode = [rightLabel] + binarize([sent[0]] + sent[2:], v=v, h=hori)[1:]
+	# return the vertically Markovized POS tag along with its two nodes
+	return [joinLabels('^', vert), leftNode, rightNode]
 
 def joinLabels(sep, list):
 	"""Joins a list, excluding empty strings.
@@ -53,5 +73,4 @@ def printTree(sent, depth):
 		print(sent[0])
 		for x in sent[1:]: printTree(x, depth + 1)
 
-print(binarize(sentence4, ['']*vValue, ['']*hValue))
-printTree(binarize(sentence4, ['']*vValue, ['']*hValue), 0)
+printTree(binarize(sentence), 0)
